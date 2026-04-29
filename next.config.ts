c8ce00b10f60265788c8d98ae64ad00bb5cbc4e1
Vladosic007 <_email@gmail.com>
2026-04-29 18:55:01 +0300

    настроил PWA для работы без интернета

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,26 @@
 import type { NextConfig } from "next";
+import withPWA from "@ducanh2912/next-pwa";
 
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    // Отключаем строгие проверки ESLint при деплое
     ignoreDuringBuilds: true,
   },
   typescript: {
-    // Отключаем строгие проверки TypeScript при деплое
     ignoreBuildErrors: true,
   },
 };
 
-export default nextConfig;
+// ❗ МАГИЯ PWA (Кэширование для работы без инета) ❗
+const withPwaConfig = withPWA({
+  dest: "public", // Куда сохранять кэш
+  cacheOnFrontEndNav: true, 
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: process.env.NODE_ENV === "development", // Отключаем кэш во время разработки, чтобы ты видел свои изменения сразу
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
+export default withPwaConfig(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import '@/app/globals.css';
 import BottomNav from "../components/BottomNav";
 
+// ❗ ЗАПРЕЩАЕМ ЗУМ (ЧТОБЫ ВЫГЛЯДЕЛО КАК НАСТОЯЩЕЕ ПРИЛОЖЕНИЕ) ❗
+export const viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru" suppressHydrationWarning>
