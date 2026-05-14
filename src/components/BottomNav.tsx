2770b9fc46733b666c264012636f0b1239f8ab7b
Vladosic007 <_email@gmail.com>
2026-05-14 14:59:22 +0300

    fix useRouter import

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,7 +1,7 @@
 'use client';
 import Link from 'next/link';
 import Image from 'next/image';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import { useState, useEffect } from 'react'; // ❗ ДОБАВИЛИ ХУКИ
 import { useCartStore } from '../store/cartStore'; // ❗ ДОБАВИЛИ ХРАНИЛИЩЕ ❗
 
