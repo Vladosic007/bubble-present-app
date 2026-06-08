f35d35b9f48d15486f4213efe3b21e953d177df8
Vladosic007 <_email@gmail.com>
2026-06-08 15:39:08 +0300

    поменял иконки

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* ❗ ПОДКЛЮЧАЕМ PWA (ЯРЛЫК НА РАБОЧИЙ СТОЛ) ❗ */}
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#FF008C" />
-        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <link rel="apple-touch-icon" href="/images/icon-192.png" />
       </head>
       <body className="antialiased bg-white text-black" suppressHydrationWarning>
 
